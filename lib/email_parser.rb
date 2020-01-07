499cb8d4d14db976a66e9b9514c43e29bc5f0523
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-07 20:36:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,9 +12,9 @@ class EmailAddressParser
   
   def parse
    emails = @address.split 
-  #  parsed_emails = emails.collect do |list|
-    #  list.split(",")
- #  end
-    emails.uniq
+   parsed_emails = emails.collect do |list|
+    list.chomp(",")
+  end
+    parsed_emails.uniq
   end
 end
